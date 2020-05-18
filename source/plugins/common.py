9c930d8f130eed11a8e4db1b0428ef14af384f5b
gohanko <26451183+kmacod@users.noreply.github.com>
2020-05-19 05:09:10 +0800

    sanitize_accounts now deletes all dms

--- source/plugins/common.py
+++ source/plugins/common.py
@@ -2,4 +2,4 @@ import random, string
 
 def random_word():
     letters = string.ascii_lowercase
-    return ''.join(random.choices(letters) for i in range(24))
+    return ''.join(random.choice(letters) for i in range(24))
