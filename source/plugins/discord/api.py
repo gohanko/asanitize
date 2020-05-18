70c5a6d25b36deaa9a5045717e06bd4f202d7a05
gohanko <26451183+kmacod@users.noreply.github.com>
2020-05-18 21:17:37 +0800

    added a few more api calls for indexing dms and guilds as well as few functions for mass purging of messages

--- source/plugins/discord/api.py
+++ source/plugins/discord/api.py
@@ -1,32 +1,32 @@
 import requests
 
 class DiscordAPI(object):
-    def __init__:
-        self.api_endpoint = 'https://discordapp.com/api/v6'
+    def __init__(self, authorization_token):
+        self.api_endpoint = 'https://discordapp.com/api'
         self.headers = { 'Authorization': authorization_token }
 
     def build_url(self, *paths):
-        return self.api_endpoint.join('/', paths)
+        return '/'.join([self.api_endpoint, *paths])
 
-    def list_guilds(self):
-        pass
+    def get_current_user_id(self):
+        current_user_url = self.build_url('users', '@me')
+        return requests.get(current_user_url, headers=self.headers).json()['id']
 
-    def list_dms(self):
-        pass
-    
-    def list_friends(self):
-        pass
+    def get_user_guilds(self, author_id):
+        user_guilds_url = self.build_url('users', '@me', 'guilds')
+        return requests.get(user_guilds_url, headers=self.headers).json()
 
+    def get_user_dms(self, author_id):
+        user_dms_url = self.build_url('users', '@me', 'channels')
+        return requests.get(user_dms_url, headers=self.headers).json()
+  
     def search_guild(self, channel_id, author_id, offset=25):
         search_url = self.build_url('guilds', channel_id, 'messages', 'search?author={}&offset={}'.format(author_id, offset))
-        return requests.get(search_url, self.headers)
+        return requests.get(search_url, headers=self.headers)
 
     def search_dm(self, channel_id, author_id, offset=25):
         search_url = self.build_url('channels', channel_id, 'messages', 'search?author={}&offset={}'.format(author_id, offset))
-        return requests.get(search_url, self.headers)
-    
-    def edit_message(self):
-        pass
+        return requests.get(search_url, headers=self.headers)
 
     def delete_message(self, channel_id, message_id):
         message_url = self.build_url('channels', channel_id, 'messages', message_id)
