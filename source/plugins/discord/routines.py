889657d815a0091e461b1d99c0c4e251b5445b4f
Brandon Yii <26451183+kmacod@users.noreply.github.com>
2020-05-23 21:20:19 +0800

    discord : refactor how searching for message is handled

--- source/plugins/discord/routines.py
+++ source/plugins/discord/routines.py
@@ -1,4 +1,5 @@
 import time
+from itertools import chain
 from ..common import random_word
 from .api import DiscordAPI
 
@@ -8,20 +9,53 @@ class DiscordRoutines(object):
         self.user_id = self.discord_api.get_current_user_id()
         self.sanitize_account()
 
-    def filter_messages_by_author(self, messages, author_id):
-        authors_messages = []
-        for message in messages:
-            if message['author']['id'] == author_id:
-                authors_messages.append(message)
+    def filter_search_result_by_user(self, message_groups, author_id):
+        if not message_groups:
+            return
 
-        return authors_messages
+        messages = list(chain.from_iterable(message_groups))
+
+        message_seen = []
+        author_messages = []
+        for message in messages:
+            if message['author']['id'] == author_id and message['id'] not in message_seen:
+                author_messages.append(message)
+                message_seen.append(message['id'])
+
+        return author_messages
+
+    def get_message_by_search_result(self, channel_id, author_id, is_guild):
+        search_page = 0
+        author_messages = []
+
+        while True:
+            response = self.discord_api.search(channel_id, author_id, search_page * 25, is_guild)
+            if response.status_code == 429:
+                time.sleep(response.retry_after)
+                continue
+            
+            filtered_messages = self.filter_search_result_by_user(response.json().get('messages'), author_id)
+            if filtered_messages:
+                author_messages.extend(filtered_messages)
+                search_page += 1
+            else:
+                break
+
+        return author_messages
 
     def sanitize_account(self):
-        dm_channels = self.discord_api.get_user_dms(self.user_id)
+        dm_channels = self.discord_api.get_user_channels(self.user_id, False)
         for dm_channel in dm_channels:
-            messages = self.discord_api.get_user_dm_messages(dm_channel['id'])
+            messages = self.get_message_by_search_result(dm_channel['id'], self.user_id, False)
 
             for message in messages:
-                if message['author']['id'] == self.user_id:
-                    self.discord_api.delete_message(message['channel_id'], message['id'])
-                    print('Deleting : {} by {}'.format(message['id'], message['author']['username']))
+                self.discord_api.delete_message(message['channel_id'], message['id'])
+                print('Deleting {}: {}'.format(message['id'], message['content']))
+        
+        guild_channels = self.discord_api.get_user_channels(self.user_id, True)
+        for guild_channel in guild_channels:
+            messages = self.get_message_by_search_result(guild_channel['id'], self.user_id, True)
+
+            for message in messages:
+                self.discord_api.delete_message(message['channel_id'], message['id'])
+                print('Deleting {}: {}'.format(message['id'], message['content']))
