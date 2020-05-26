de21540f4b2bfb8abb48797df3c2081684d92910
gohanko <26451183+kmacod@users.noreply.github.com>
2020-05-26 18:52:35 +0800

    update requirements.txt

--- source/cmd.py
+++ source/cmd.py
@@ -10,44 +10,42 @@
 import argparse
 from .plugins.discord.routines import DiscordRoutines
 
-COMMANDS = [
-    {
-        'short_arg': '-t',
-        'long_arg': '--token',
-        'named_parameters': {
-            'type': str,
-            'required': True,
-            'help': 'The authorization token to access the account.'
-        },
-    },
-    {
-        'short_arg': '-s',
-        'long_arg': '--service',
-        'named_parameters': {
-            'type': str,
-            'required': True,
-            'help': 'The service in which the account you want to sanitize resides.'
-        },
-    }
-]
-
-ROUTINES = [
-    {
-        'service': 'discord',
-        'routine': DiscordRoutines
-    }
-]
-
 if __name__ == '__main__':
-    PARSER = argparse.ArgumentParser()
+    COMMANDS = [
+        {
+            'short_arg': '-t',
+            'long_arg': '--token',
+            'named_parameters': {
+                'type': str,
+                'required': True,
+                'help': 'The authorization token to access the account.'
+            },
+        },
+        {
+            'short_arg': '-s',
+            'long_arg': '--service',
+            'named_parameters': {
+                'type': str,
+                'required': True,
+                'help': 'The service in which the account you want to sanitize resides.'
+            },
+        }
+    ]
 
+    ROUTINES = [
+        {
+            'service': 'discord',
+            'routine': DiscordRoutines
+        }
+    ]
+
+    PARSER = argparse.ArgumentParser()
     for COMMAND in COMMANDS:
         PARSER.add_argument(
             COMMAND['short_arg'],
             COMMAND['long_arg'],
             **COMMAND['named_parameters']
         )
-
 
     ARGS = PARSER.parse_args()
     for ROUTINE in ROUTINES:
