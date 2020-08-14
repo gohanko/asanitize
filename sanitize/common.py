61363d5bdd694d094a9651077f16b939823b4456
gohanko <26451183+gohanko@users.noreply.github.com>
2020-08-15 03:31:21 +0800

    added common test

--- sanitize/common.py
+++ sanitize/common.py
@@ -6,7 +6,6 @@ import logging.config
 import yaml
 
 def random_word():
-    """Generates a random word at the length of 2000 characters"""
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(2000))
 
