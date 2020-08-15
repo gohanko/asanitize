a62503f59fe21f4805b647cdde524200095da405
Brandon <26451183+gohanko@users.noreply.github.com>
2020-08-15 12:14:40 +0800

    Add support to load from file instead of just commandline

--- sanitize/commands/discord_command.py
+++ sanitize/commands/discord_command.py
@@ -1,5 +1,6 @@
 from cleo import Command
 
+from sanitize.common import load_yml
 from sanitize.errors import ArgumentIsNotFound
 from sanitize.plugins.discord.routines import DiscordRoutines
 
@@ -8,18 +9,30 @@ class DiscordCommand(Command):
     Commands to sanitize a discord account.
 
     discord
-        {--t|token= : Must be set. It is used to authenticated into a particular account.}
+        {--t|token=? : Must be set. It is used to authenticated into a particular account.}
         {--c|channel=? : If set, it will only clean that particular channel.}
         {--a|all : If set, it will clean all the channels found in a discord account.}
+        {--f|file=? : Config file so users don't have to type everything.}
     """
     def handle(self):
-        token = self.option('token')
-        if not token:
-            raise ArgumentIsNotFound('token')
+        config_file = self.option('file')
+        if config_file:
+            config = load_yml(self.option('file')['discord'])
+        else:
+            config = {
+                'token': self.option('token'),
+                'channel': self.option('channel'),
+                'all': self.option('all'),
+            }
 
-        channel_id = self.option('channel')
-        if channel_id:
-            DiscordRoutines(token).sanitize_channel(channel_id)
+        self.execute(**config)
+
+    def execute(self, token, channel, all):
+        if not token:
+            raise ArgumentIsNotFound('Token is not found')
+
+        if channel:
+            DiscordRoutines(token).sanitize_channel(channel)
         
-        if self.option('all'):
+        if all:
             DiscordRoutines(token).sanitize_account()
--- test/test_common.py
+++ test/test_common.py
@@ -2,6 +2,7 @@ import unittest
 
 from sanitize.common import (
     random_word,
+    load_yml,
     create_logger,
 )
 
@@ -9,6 +10,10 @@ class TestCommon(unittest.TestCase):
     def test_random_word(self):
         words = random_word()
         self.assertEqual(len(words), 2000)
+
+    def test_load_yml(self):
+        data = load_yml('./config/test.config.yml')
+        self.assertEqual(isinstance(data, object), True)
 
     def test_create_logger(self):
         logger = create_logger('test_logger')
