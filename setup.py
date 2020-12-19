194dc48057d37d263eb740010fa0e26451dc0518
gohanko <26451183+gohanko@users.noreply.github.com>
2020-12-19 10:41:29 +0800

    Fix readme and use text/markdown for long description content type

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open('docs/README.md', 'r', encoding='UTF-8') as fh:
 
 setuptools.setup(
     name='asanitize',
-    version='0.0.4',
+    version='0.0.5',
     author='Brandon',
     author_email='pleasecontactmeongithub@localhost.local',
     description='A simple commandline tool to bulk delete messages/posts from your social media accounts.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/gohanko/asanitize',
     packages=setuptools.find_packages(include=['asanitize']),
     install_requires=[
