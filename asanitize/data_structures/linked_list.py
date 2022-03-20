c27c7b4544c805a7b2ba8c37dfe86294524f5451
gohanko <26451183+gohanko@users.noreply.github.com>
2022-03-20 15:30:16 +0800

    Added LinkedList implementation

---
+++ asanitize/data_structures/linked_list.py
@@ -0,0 +1,74 @@
+from email import header
+
+
+class Node:
+    item = None
+    prev = None
+    next = None
+
+    def __init__(self, prev, item, next):
+        self.prev = prev
+        self.item = item
+        self.next = next
+    
+class LinkedList:
+    count = 0
+    head = None
+
+    def is_empty(self):
+        return (self.count == 0)
+
+    def find(self, position):
+        if position > self.count or self.is_empty():
+            return None
+
+        current_node = self.head
+        for i in range(0, self.count):
+            if i == position:
+                break
+
+            current_node = current_node.next
+
+        return current_node
+    
+    def set(self, position, item):
+        if position > self.count:
+            return False
+
+        node = self.find(position)
+        node.item = item
+        return True
+
+    def insert(self, position, item):
+        if position > self.count:
+            return False
+
+        # If it is adding at the start of list.
+        if position == 0:
+            self.head = Node(None, item, self.head)
+        elif position == self.count: # If it is adding at the end of list.
+            prev_node = self.find(position - 1)
+            new_node = Node(prev_node, item, None)
+            prev_node.next = new_node
+        else: # If it is adding at anywhere between 2 nodes.
+            node_to_replace = self.find(position)
+            new_node = Node(node_to_replace.prev, item, node_to_replace)
+            node_to_replace.prev.next = new_node
+            node_to_replace.prev = new_node
+
+        self.count += 1
+        return True
+
+    def append(self, item):
+        return self.insert(self.count, item)
+        
+    def remove(self, position):
+        if position > self.count:
+            return False
+        
+        node = self.find(position)
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        return True
---
+++ tests/data_structures/test_linked_list.py
@@ -0,0 +1,51 @@
+import unittest
+from asanitize.data_structures.linked_list import Node, LinkedList
+
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.linked_list = LinkedList()
+
+    def test_isempty_if_empty(self) -> None:
+        self.assertTrue(self.linked_list.is_empty())
+
+    def test_insert(self) -> None:
+        self.linked_list.append('test_item')
+        self.linked_list.append('test_item2')
+        self.linked_list.append('test_item3')
+
+        self.assertEqual(self.linked_list.find(0).item, 'test_item')
+        self.linked_list.insert(1, 'aaaa')
+        self.assertEqual(self.linked_list.find(1).item, 'aaaa')
+        self.assertEqual(self.linked_list.find(2).item, 'test_item2')
+        self.assertEqual(self.linked_list.find(3).item, 'test_item3')
+
+    def test_append(self) -> None:
+        self.linked_list.append('test_item')
+        self.assertEqual(self.linked_list.head.item, 'test_item')
+
+    def test_isempty_if_not_empty(self) -> None:
+        self.linked_list.append('test_item')
+        self.assertFalse(self.linked_list.is_empty())
+
+    def test_find(self) -> None:
+        self.linked_list.append('test_item')
+        node = self.linked_list.find(0)
+
+        self.assertEqual(node.item, 'test_item')
+
+    def test_set(self) -> None:
+        self.linked_list.append('test_item')
+        node = self.linked_list.find(0) 
+        
+        self.assertEqual(node.item, 'test_item')
+        self.assertTrue(self.linked_list.set(0, 'test_item_2'))
+        self.assertEqual(node.item, 'test_item_2')
+
+    def test_remove(self):
+        self.linked_list.append('test_item')
+        self.linked_list.append('test_item2')
+        self.linked_list.append('test_item3')
+
+        self.linked_list.remove(1)
+        self.assertEqual(self.linked_list.find(1).item, 'test_item3')
